02a03ead2a1fa24dd6fbd74145fc773d20ae14bb
Yuchen Zhang <yuchen.zhang@ericsson.com>
2017-12-07 14:17:07 +0800

    Split draw function into this Drawer module

---
+++ Drawer.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Drawer:
+    def __init__(self, surface):
+        self._surface = surface
+
+    def draw_field():
+        pass
+    
+    def draw_text():
+        pass
+
+    def draw_content():
+        pass
