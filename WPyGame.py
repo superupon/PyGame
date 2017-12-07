15470da73a91e7e02926f7d24476ba280761b2d5
Yuchen Zhang <yuchen.zhang@ericsson.com>
2017-12-07 09:38:31 +0800

    Add logic to handle rotate, left, and right

--- WPyGame.py
+++ WPyGame.py
@@ -13,32 +13,38 @@ class WindowField:
         print "-" + 3*self._column*"-" + "-"
 
 if __name__ == "__main__":
-   pygame.init()
-   clock = pygame.time.Clock() 
-   window = pygame.display.set_mode((640, 480))
-   x = 0
-   y = 0
-   count = 0
-   pygame.display.set_caption("PyGame")
-   content = Content(12, 6)
-   while True:
-       for event in pygame.event.get():
-           if event.type == QUIT:
-               pygame.quit()
-           if event.type == KEYDOWN:
-               if event.__dict__['key'] == K_UP:
-                   print 'add'
-                   content.add_line(2, [1,2])
+    pygame.init()
+    clock = pygame.time.Clock() 
+    window = pygame.display.set_mode((640, 480))
+    x = 0
+    y = 0
+    count = 0
+    pygame.display.set_caption("PyGame")
+    content = Content(12, 6)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.__dict__['key'] == K_UP:
+                    print 'add'
+                    content.add_line(2, [1,2])
+                if event.__dict__['key'] == K_r:
+                    content.rotate()
+                if event.__dict__['key'] == K_LEFT:
+                    content.left()
+                if event.__dict__['key'] == K_RIGHT:
+                    content.right()
 
-       #window.fill((60, 100, 200))
-       pygame.draw.rect(window, (255,155,255), (200, 50, 200, 400), 10)
-       pygame.display.update()
-       clock.tick(30)
-       count = count + 1
-       if count == 100:
-           print "step"
-           count = 0
-           content.step()
-           content.printContent()
-       content.drawContent(window)
-
+        #window.fill((60, 100, 200))
+        pygame.draw.rect(window, (255,155,255), (200, 50, 200, 400), 10)
+        pygame.display.update()
+        clock.tick(30)
+        count = count + 1
+        if count == 100:
+            print "step"
+            count = 0
+            content.step()
+            content.printContent()
+        content.drawContent(window)
